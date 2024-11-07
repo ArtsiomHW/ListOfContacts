ef6d839e973397c1e1e03c24847f6881ff2a6bf6
Artsiom <artsiom.hurynovich@gmail.com>
2024-11-07 22:52:29 +0100

    Changed logic of randon contact

--- ListOfContacts/Contact.swift
+++ ListOfContacts/Contact.swift
@@ -12,29 +12,34 @@ struct Contact {
     let surname: String
     let phoneNumber: String
     let email: String
-
+    
     static func getUniqueContact(name: [String], surname: [String], phoneNumber: [String], email: [String]) -> [Contact] {
- 
-        var usedIndex: Set<Int> = []
-        var fullContact: [Contact] = []
-        
-        while fullContact.count < name.count {
-            let randomIndex = Int.random(in: 0..<name.count)
+            var usedCombinations: [[String]] = []
+            var fullContact: [Contact] = []
             
-            if !usedIndex.contains(randomIndex) {
-                usedIndex.insert(randomIndex)
-                fullContact.append(
-                    Contact(
-                        name: name[randomIndex],
-                        surname: surname[randomIndex],
-                        phoneNumber: phoneNumber[randomIndex],
-                        email: email[randomIndex]
-                    )
+            while fullContact.count < min(name.count, surname.count, phoneNumber.count, email.count) {
+                let randomNameIndex = Int.random(in: 0..<name.count)
+                let randomSurnameIndex = Int.random(in: 0..<surname.count)
+                let randomPhoneIndex = Int.random(in: 0..<phoneNumber.count)
+                let randomEmailIndex = Int.random(in: 0..<email.count)
+                
+                let newContact = Contact(
+                    name: name[randomNameIndex],
+                    surname: surname[randomSurnameIndex],
+                    phoneNumber: phoneNumber[randomPhoneIndex],
+                    email: email[randomEmailIndex]
                 )
+                
+                let combination = [newContact.name, newContact.surname, newContact.phoneNumber, newContact.email]
+                
+                if !usedCombinations.contains(combination) {
+                    usedCombinations.append(combination)
+                    fullContact.append(newContact)
+                }
             }
-        }
         
-        return fullContact
-    }
+            return fullContact
+        }
+    
 }
 
